0a556bef24144a2f8b2cf1a382b8701682cd49a5
William Li <wul@microsoft.com>
2020-10-21 22:23:27 -0700

    can compile

--- Program.cs
+++ Program.cs
@@ -4,47 +4,61 @@ using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
+using NuGet.Test.Utility;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Frameworks;
+using NuGet.LibraryModel;
+using NuGet.Packaging;
+using NuGet.ProjectModel;
+using NuGet.Versioning;
 
 namespace sdkbootstrapper
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using var cacheContext = new SourceCacheContext();
-                var restoreContext = new RestoreArgs()
+            using var pathContext = new SimpleTestPathContext();
+            var providerCache = new RestoreCommandProvidersCache();
+            var dgFile = new DependencyGraphSpec();
+            var sources = new List<string>() {pathContext.PackageSource};
+            var restoreContext = new RestoreArgs()
             {
                 CacheContext = cacheContext,
                 DisableParallel = true,
-                GlobalPackagesFolder = Path.Combine("c:\\work", "globalPackages"),
-                Sources = new List<string>() { "C:\\work\\temp-cswinrt\\feed" } ,
+                GlobalPackagesFolder = pathContext.UserPackagesFolder,
+                Sources = sources,
                 Log = new NullLogger(),
-                CachingSourceProvider = new CachingSourceProvider(new TestPackageSourceProvider(new List<PackageSource>() { new PackageSource(pathContext.PackageSource) })),
+                CachingSourceProvider =
+                    new CachingSourceProvider(new TestPackageSourceProvider(
+                        new List<PackageSource>() {new PackageSource(pathContext.PackageSource)})),
                 PreLoadedRequestProviders = new List<IPreLoadedRestoreRequestProvider>()
-                    {
-                        new DependencyGraphSpecRequestProvider(providerCache, dgFile)
-                    }
+                {
+                    new DependencyGraphSpecRequestProvider(providerCache, dgFile)
+                }
             };
 
             var request = (await RestoreRunner.GetRequests(restoreContext)).Single();
-            var providers = providerCache.GetOrCreate(pathContext.UserPackagesFolder, sources, new List<SourceRepository>(), cacheContext, logger);
+            var providers = providerCache.GetOrCreate(pathContext.UserPackagesFolder, sources,
+                new List<SourceRepository>(), cacheContext, new NullLogger());
             var command = new RestoreCommand(request.Request);
 
             // Add to cache before install on all providers
             var globalPackages = providers.GlobalPackages;
             var packages = globalPackages.FindPackagesById("a");
-            packages.Should().BeEmpty("has not been installed yet");
 
             foreach (var local in providers.LocalProviders)
             {
-                await local.GetDependenciesAsync(new LibraryIdentity("a", NuGetVersion.Parse("1.0.0"), LibraryType.Package), NuGetFramework.Parse("net46"), cacheContext, logger, CancellationToken.None);
+                await local.GetDependenciesAsync(
+                    new LibraryIdentity("a", NuGetVersion.Parse("1.0.0"), LibraryType.Package),
+                    NuGetFramework.Parse("net46"), cacheContext, new NullLogger(), CancellationToken.None);
             }
-
-            // Install the package without updating the cache
-            await SimpleTestPackageUtility.CreateFolderFeedV3Async(pathContext.UserPackagesFolder, PackageSaveMode.Defaultv3, packageA);
 
             // Run restore using an incorrect cache
             var result = await command.ExecuteAsync();
---
+++ SimpleTestSettingsContext.cs
@@ -0,0 +1,179 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using NuGet.Common;
+
+namespace NuGet.Test.Utility
+{
+    public class SimpleTestSettingsContext
+    {
+        /// <summary>
+        /// NuGet.Config path on disk
+        /// </summary>
+        public string ConfigPath { get; }
+
+        /// <summary>
+        /// XML
+        /// </summary>
+        public XDocument XML { get; }
+
+
+        public SimpleTestSettingsContext(string nugetConfigPath, string userPackagesFolder, string packagesV2, string fallbackFolder, string packageSource)
+            : this(nugetConfigPath, GetDefault(userPackagesFolder, packagesV2, fallbackFolder, packageSource))
+        {
+        }
+
+        public SimpleTestSettingsContext(string path, XDocument xml)
+        {
+            XML = xml ?? throw new ArgumentNullException(nameof(xml));
+            ConfigPath = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        /// <summary>
+        /// Save to disk
+        /// </summary>
+        public void Save()
+        {
+            FileUtility.Replace((outputPath) =>
+            {
+                using (var output = new FileStream(outputPath, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                {
+                    XML.Save(output);
+                }
+            },
+            ConfigPath);
+        }
+
+        /// <summary>
+        /// Disable automatic package restore and save the file.
+        /// </summary>
+        public void DisableAutoRestore()
+        {
+            var section = GetOrAddSection(XML, "packageRestore");
+
+            AddEntry(section, "enabled", "false");
+            AddEntry(section, "automatic", "false");
+
+            Save();
+        }
+
+        private static XDocument GetDefault(string userPackagesFolder, string packagesV2, string fallbackFolder, string packageSource)
+        {
+            var doc = GetEmptyConfig();
+
+            var packageSources = GetOrAddSection(doc, "packageSources");
+            AddEntry(packageSources, "source", packageSource);
+
+            var fallbackFolders = GetOrAddSection(doc, "fallbackPackageFolders");
+            AddEntry(fallbackFolders, "shared", fallbackFolder);
+
+            AddSetting(doc, "globalPackagesFolder", userPackagesFolder);
+            AddSetting(doc, "repositoryPath", packagesV2);
+
+            return doc;
+        }
+
+        private static XDocument GetEmptyConfig()
+        {
+            var doc = new XDocument();
+            var configuration = new XElement(XName.Get("configuration"));
+            doc.Add(configuration);
+
+            var config = GetOrAddSection(doc, "config");
+            var packageSources = GetOrAddSection(doc, "packageSources");
+            var disabledSources = GetOrAddSection(doc, "disabledPackageSources");
+            var fallbackFolders = GetOrAddSection(doc, "fallbackPackageFolders");
+
+            packageSources.Add(new XElement(XName.Get("clear")));
+            disabledSources.Add(new XElement(XName.Get("clear")));
+
+            return doc;
+        }
+
+        public static XElement GetOrAddSection(XDocument doc, string name)
+        {
+            var root = doc.Element(XName.Get("configuration"));
+
+            var node = root.Element(XName.Get(name));
+
+            if (node == null)
+            {
+                node = new XElement(XName.Get(name));
+                root.Add(node);
+            }
+
+            return node;
+        }
+
+        public static void AddEntry(XElement section, string key, string value)
+        {
+            var setting = new XElement(XName.Get("add"));
+            setting.Add(new XAttribute(XName.Get("key"), key));
+            setting.Add(new XAttribute(XName.Get("value"), value));
+            section.Add(setting);
+        }
+
+        public static void AddSetting(XDocument doc, string key, string value)
+        {
+            RemoveSetting(doc, key);
+            var config = GetOrAddSection(doc, "config");
+
+            var setting = new XElement(XName.Get("add"));
+            setting.Add(new XAttribute(XName.Get("key"), key));
+            setting.Add(new XAttribute(XName.Get("value"), value));
+            config.Add(setting);
+        }
+
+        public static void RemoveSetting(XDocument doc, string key)
+        {
+            var config = GetOrAddSection(doc, "config");
+
+            foreach (var item in config.Elements(XName.Get("add")).Where(e => e.Name.LocalName.Equals(key, StringComparison.OrdinalIgnoreCase)).ToArray())
+            {
+                item.Remove();
+            }
+        }
+
+        // Add NetStandard.Library and NetCorePlatforms to the feed and save the file.
+        public void AddNetStandardFeeds()
+        {
+            var reposRoot = GetRepositoryRootDirectory();
+            var netStandardLibraryPackageFeed = GetRepoPackageDirectoryPath(reposRoot, "netstandard.library");
+            var netCorePlatformsPackageFeed = GetRepoPackageDirectoryPath(reposRoot, "microsoft.netcore.platforms");
+
+            var section = GetOrAddSection(XML, "packageSources");
+            AddEntry(section, "NetStandardLibrary", netStandardLibraryPackageFeed);
+            AddEntry(section, "NetCorePlatforms", netCorePlatformsPackageFeed);
+
+            Save();
+        }
+
+        private static DirectoryInfo GetRepositoryRootDirectory()
+        {
+            DirectoryInfo currentDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            while (currentDir != null)
+            {
+                if (File.Exists(Path.Combine(currentDir.FullName, "NuGet.sln")))
+                {
+                    // We have found the repo root.
+                    return currentDir;
+                }
+
+                currentDir = currentDir.Parent;
+            }
+
+            throw new DirectoryNotFoundException($"Starting from {Directory.GetCurrentDirectory()} the directory containing 'NuGet.sln' could not be found.");
+        }
+
+        private static string GetRepoPackageDirectoryPath(DirectoryInfo reposRoot, string packageId)
+        {
+            var repoPackageDir = Path.Combine(reposRoot.FullName, "packages", packageId);
+            return repoPackageDir;
+        }
+
+    }
+}
